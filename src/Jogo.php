6395535b50e2487223f3546cfa6b2ec1b8edd763
Nobru <brunobacus@gmail.com>
2014-02-21 14:17:14 -0300

    caso 3 - 2

--- src/Jogo.php
+++ src/Jogo.php
@@ -3,9 +3,15 @@
 class Jogo
 {
     public $pontos = 0;
+    public $jogadas = 0;
 
     public function lancar($jogada1, $jogada2)
     {
+        if ($this->jogadas == 10) {
+            throw new Exception("Quantidade de jogadas excedido");
+        }
+
         $this->pontos+= $jogada1 + $jogada2;
+        $this->jogadas++;
     }
 }
