d3af87a806df8ea3f9f11148844afc5c8b184644
Nobru <brunobacus@gmail.com>
2014-02-21 15:05:29 -0300

    caso 4 - 2

--- src/Jogo.php
+++ src/Jogo.php
@@ -16,14 +16,24 @@ class Jogo
 
         $lance = new Lance;
         $lance->adicionar($jogada1, $jogada2);
+        $this->lances[] = $lance;
 
-        $this->pontos+= $jogada1 + $jogada2;
         $this->jogadas++;
     }
 
     public function getPontos()
     {
-        foreach ($this->lances as $numero->$lance) {
+        foreach ($this->lances as $numero=>$lance) {
+            
+            if ($numero > 0) {
+                $lanceAnterior = $this->lances[$numero-1];
+
+                if ($lanceAnterior->isSpare) {
+                    $this->pontos+= ($lance->jogada1 * 2) + $lance->jogada2;
+                    continue;
+                }
+            }
+
             $this->pontos+= $lance->jogada1 + $lance->jogada2;
         }
 
